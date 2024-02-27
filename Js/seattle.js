e932d30f5b41437cf7d8fad892a04309463c705c
Amanda <amandamarquez27@icloud.com>
2024-02-27 00:03:36 -0800

    add object content

---
+++ Js/seattle.js
@@ -0,0 +1,30 @@
+
+let seattle = {
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgCookiesPerSale: 6.3,
+    hourlySales: [],
+    generateHourlySales: function() {
+        for (let hour = 6; hour <= 19; hour++) {
+            let customers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
+            let cookiesSold = Math.round(customers * this.avgCookiesPerSale);
+            this.hourlySales.push(cookiesSold);
+        }
+    },
+    render: function() {
+        let totalCookies = 0;
+        let salesList = document.getElementById('seattle-sales');
+        for (let hour = 6; hour <= 19; hour++) {
+            let cookies = this.hourlySales[hour - 6];
+            totalCookies += cookies;
+            let listItem = document.createElement('li');
+            listItem.textContent = hour + ':00: ' + cookies + ' cookies';
+            salesList.appendChild(listItem);
+        }
+        let totalItem = document.createElement('li');
+        totalItem.textContent = 'Total: ' + totalCookies + ' cookies';
+        salesList.appendChild(totalItem);
+    }
+}
+seattle.generateHourlySales();
+seattle.render();
